8f2981f1f1c2011e36aba21aee325c0d5000eafb
Aleksei Orlov <alexeyorlov65@gmail.com>
2024-09-12 19:45:43 +0300

    lab_01: generate fake data

--- lab_01/write_csv.py
+++ lab_01/write_csv.py
@@ -1,4 +1,5 @@
 import csv
+from os import makedirs
 
 from fake_course import Course
 from fake_enrollment import Enrollment
@@ -8,7 +9,7 @@ from fake_teacher import Teacher
 
 def write_students(students: list[Student]) -> None:
     print('Writing students...')
-    with open('students.csv', 'w') as file:
+    with open('data/students.csv', 'w') as file:
         writer = csv.writer(file)
         for student_list in map(lambda el: el.list(), students):
             writer.writerow(student_list)
@@ -16,7 +17,7 @@ def write_students(students: list[Student]) -> None:
 
 def write_teachers(teachers: list[Teacher]) -> None:
     print('Writing teachers...')
-    with open('teachers.csv', 'w') as file:
+    with open('data/teachers.csv', 'w') as file:
         writer = csv.writer(file)
         for teacher_list in map(lambda el: el.list(), teachers):
             writer.writerow(teacher_list)
@@ -24,7 +25,7 @@ def write_teachers(teachers: list[Teacher]) -> None:
 
 def write_courses(courses: list[Course]) -> None:
     print('Writing courses...')
-    with open('courses.csv', 'w') as file:
+    with open('data/courses.csv', 'w') as file:
         writer = csv.writer(file)
         for course_list in map(lambda el: el.list(), courses):
             writer.writerow(course_list)
@@ -32,7 +33,10 @@ def write_courses(courses: list[Course]) -> None:
 
 def write_enrollments(enrollments: list[Enrollment]) -> None:
     print('Writing enrollments...')
-    with open('enrollments.csv', 'w') as file:
+    with open('data/enrollments.csv', 'w') as file:
         writer = csv.writer(file)
         for enrollment_list in map(lambda el: el.list(), enrollments):
             writer.writerow(enrollment_list)
+
+
+makedirs('data', exist_ok=True)
